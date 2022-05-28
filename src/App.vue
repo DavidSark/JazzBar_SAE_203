69f00d0df19c44d64cd7ad632d639e055a4796a6
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-05-28 18:32:39 +0200

    Création de la page

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    
+    
+    <nav>
+      <RouterLink to="/" class="text-blue-600 underline">Accueil</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+/* Import des composents*/
+
+/* Import d'heroicons si besoin*/
+import { } from "@heroicons/vue/outline";
+
+/* Appel des composents */
+export default {
+  components: { },
+};
+</script>
---
+++ src/views/AccueilView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <h1 class="text-2xl">Page d'accueil</h1>
+    <MenuIcon class="w-10 text-white "></MenuIcon>
+  </main>
+</template>
+
+<script>
+/* Import des composents*/
+
+
+/* Import d'heroicons si besoin*/
+import { MenuIcon } from "@heroicons/vue/outline";
+
+/* Appel des composents */
+export default {
+  components: { MenuIcon, },
+};
+</script>
